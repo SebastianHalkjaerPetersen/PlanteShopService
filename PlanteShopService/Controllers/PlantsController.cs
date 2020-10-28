c7625e17de9ef0b2dc24d8b8bed59df4c4cfa3a4
Sebastian Halkjær Petersen <seba449h@edu.easj.dk>
2020-10-28 13:04:25 +0100

    Add project files.

---
+++ PlanteShopService/Controllers/PlantsController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PlanteShopService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlantsController : ControllerBase
+    {
+
+        public static List<Plante> PlantList = new List<Plante>()
+        {
+            new Plante(1, "Rose", "Albertine", 400, 199),
+            new Plante(2, "Busk", "Aronia", 200, 169),
+            new Plante(3, "FrugtOgBaer", "AromaAeble", 350, 399)
+        };
+        // GET: api/Plants
+        [HttpGet]
+        public IEnumerable<Plante> GetAllPlants()
+        {
+            return PlantList;
+        }
+
+        // GET: api/Plants/5
+        [HttpGet]
+        [Route("{id}")]
+        public Plante GetPlantByID(int id)
+        {
+            return PlantList.Find(i => i.PlanteID == id);
+        }
+
+
+        // POST: api/Plants 
+        [HttpPost]
+        public int AddPlant(Plante plant)
+        {
+            int NewID = PlantList.Max(c => c.PlanteID) + 1;
+            plant.PlanteID = NewID;
+            PlantList.Add(plant);
+            return plant.PlanteID;
+        }
+
+        [HttpGet]
+        [Route("Type/{type}")]
+        public IEnumerable<Plante> GetPlantByType(string type)
+        {
+
+            return PlantList.FindAll(i => i.PlanteType.Contains(type));
+        }
+
+
+    }
+}
---
+++ PlanteShopService/Plante.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlanteShopService
+{
+    public class Plante
+    {
+		private int _planteID;
+
+		public int PlanteID
+		{
+			get { return _planteID; }
+			set { _planteID = value; }
+		}
+
+
+		private string _planteType;
+
+		public string PlanteType
+		{
+			get { return _planteType; }
+			set { _planteType = value; }
+		}
+
+
+		private string _planteNavn;
+
+		public string PlanteNavn
+		{
+			get { return _planteNavn; }
+			set { _planteNavn = value; }
+		}
+
+
+		private int _maksHoejde;
+
+		public int MaksHoejde
+		{
+			get { return _maksHoejde; }
+			set { _maksHoejde = value; }
+		}
+
+		private double _price;
+
+		public double Price
+		{
+			get { return _price; }
+			set { _price = value; }
+		}
+
+
+        public Plante(int planteId, string planteType, string planteNavn, int maksHoejde, double price)
+        {
+            _planteID = planteId;
+            _planteType = planteType;
+            _planteNavn = planteNavn;
+            _maksHoejde = maksHoejde;
+            _price = price;
+        }
+
+        public Plante(string planteType, string planteNavn, int maksHoejde, double price)
+        {
+            _planteType = planteType;
+            _planteNavn = planteNavn;
+            _maksHoejde = maksHoejde;
+            _price = price;
+        }
+
+        public Plante()
+        {
+            
+        }
+
+        public override string ToString()
+        {
+            return $"{PlanteID}, {PlanteType}, {PlanteNavn}, {MaksHoejde}, {Price}";
+
+
+        }
+
+	}
+}
